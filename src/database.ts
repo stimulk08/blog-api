a3d9212120e9c4fb93b05df4b47b9ffc0f50ff3b
stimulk08 <63906786+stimulk08@users.noreply.github.com>
2022-07-19 19:09:46 +0500

    FEATURE  * Add post model  * Add db open, close connection

--- src/database.ts
+++ src/database.ts
@@ -3,7 +3,7 @@ import {
   DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD, DB_LOG,
 } from './config';
 
-export default new Sequelize({
+const database = new Sequelize({
   dialect: 'postgres',
   logging: DB_LOG ? console.log : false,
   host: DB_HOST,
@@ -12,3 +12,13 @@ export default new Sequelize({
   username: DB_USER,
   password: DB_PASSWORD,
 });
+
+export default database;
+
+export function openConnection() {
+  return database.authenticate();
+}
+
+export function closeConnection() {
+  return database.close();
+}
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,5 +1,6 @@
 import { Model, DataTypes } from 'sequelize';
 import database from '../database';
+import { PostModel } from './post';
 
 export default class User extends Model {
   public id?: number;
@@ -10,27 +11,23 @@ export default class User extends Model {
 
   public country?: string;
 }
-
-export const UserModel = User.init({
+const UserModel = User.init({
   id: {
     type: DataTypes.INTEGER,
     autoIncrement: true,
     primaryKey: true,
   },
-  name: {
-    type: DataTypes.STRING(255),
-  },
-  password: {
-    type: DataTypes.STRING(100),
-  },
-  country: {
-    type: DataTypes.STRING(50),
-    allowNull: true,
-  },
+  name: { type: DataTypes.STRING(255) },
+  password: { type: DataTypes.STRING(100) },
+  country: { type: DataTypes.STRING(50), allowNull: true },
 }, {
   sequelize: database,
   modelName: 'User',
   tableName: 'users',
   timestamps: false,
 });
+
+UserModel.hasMany(PostModel, { foreignKey: 'author_id' });
 User.sync();
+
+export { UserModel };
